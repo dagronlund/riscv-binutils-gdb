ea8cbbbb63f17ab7d9f243cd0f4008897cb4c443
g-ram <ggobieski@yahoo.com>
2019-03-07 17:18:53 -0500

    reworked encoding, masking, and added scalar reads

--- gas/config/tc-riscv.c
+++ gas/config/tc-riscv.c
@@ -475,8 +475,7 @@ static bfd_boolean validate_riscv_insn(const struct riscv_opcode *opc) {
 	while (*p)
 		switch (c = *p++) {
 		case 'V': /* RVV */
-			USE_BITS(OP_MASK_AQ, OP_SH_AQ);
-			USE_BITS(OP_MASK_RL, OP_SH_RL);
+			USE_BITS(OP_MASK_VM, OP_SH_VM);
 			switch (c = *p++) {
 				case 's':
 					USE_BITS(OP_MASK_RS1, OP_SH_RS1);
@@ -487,13 +486,9 @@ static bfd_boolean validate_riscv_insn(const struct riscv_opcode *opc) {
 				case 't':
 					USE_BITS(OP_MASK_RS2, OP_SH_RS2);
 					break;
-				case 'm':
-					USE_BITS(OP_MASK_RS3, OP_SH_RS3);
-					break;
 				default:
 				as_bad(_("internal: bad RISC-V opcode (unknown operand type `C%c'): %s "
-								 "%s"),
-							 c, opc->name, opc->args);
+					 "%s"), c, opc->name, opc->args);
 				return FALSE;
 			}
 			break;
@@ -1214,7 +1209,6 @@ static const char *riscv_ip(char *str, struct riscv_cl_insn *ip,
 	unsigned int regno;
 	char save_c = 0;
 	int argnum;
-	char is_scalar = 0;
 	const struct percent_op_match *p;
 	const char *error = "unrecognized opcode";
 
@@ -1247,7 +1241,6 @@ static const char *riscv_ip(char *str, struct riscv_cl_insn *ip,
 			case '\0': /* End of args.  */
 				if (insn->pinfo != INSN_MACRO) {
 					if (!insn->match_func(insn, ip->insn_opcode)) {
-						printf("NO MATCH\n");
 						break;
 					}
 					if (riscv_insn_length(insn->match) == 2 && !riscv_opts.rvc)
@@ -1276,25 +1269,14 @@ static const char *riscv_ip(char *str, struct riscv_cl_insn *ip,
 						char prev = '\0';
 						for(unsigned int i = 0; i < 2; i++) {
 							if(*s != '.') break;
-							switch(*(s + 1)) {
-								case 'k':
-									if(prev == 'k') {
-										err = 1;
-										break;
-									}
-									OR_BITS((*ip).insn_opcode, 0x20, OP_MASK_RD, OP_SH_RD);
-									break;
-								case 's':
-									if(prev == 's') {
-										err = 1;
-										break;
-									}
-									is_scalar = 1;
-									INSERT_BITS((*ip).insn_opcode, 0x1, OP_MASK_RL, OP_SH_RL);
-									break;
-								default: err = 1;
+							if(*(s + 1) == 'k' && prev != 'k') {
+								OR_BITS((*ip).insn_opcode, 0x20, OP_MASK_RD, OP_SH_RD);
+							} else if(*(s + 1) == 'm' && prev != 'm') {
+								INSERT_BITS((*ip).insn_opcode, 0x1, OP_MASK_VM, OP_SH_VM);
+							} else {
+								err = 1;
+								break;
 							}
-							if(err) break;
 							prev = *(s + 1);
 							s += 2;
 						}
@@ -1309,47 +1291,6 @@ static const char *riscv_ip(char *str, struct riscv_cl_insn *ip,
 						if(*s == '.' && *(s + 1) == 'k') {
 							s += 2;
 							OR_BITS((*ip).insn_opcode, 0x20, OP_MASK_RS2, OP_SH_RS2);
-						}
-						continue;
-					case 'm':
-						if (!reg_lookup(&s, RCLASS_VECR, &regno)) break;
-						INSERT_OPERAND(RS3, *ip, regno);
-						err = 0;
-						prev = '\0';
-						for(unsigned int i = 0; i < 2; i++) {
-							if(*s != '.') break;
-							switch(*(s + 1)) {
-								case 'k':
-									if(prev == 'k') {
-										err = 1;
-										break;
-									}
-									OR_BITS((*ip).insn_opcode, 0x20, OP_MASK_RS2, OP_SH_RS2);
-									break;
-								case 't':
-									if(prev == 't' || prev == 'f' || is_scalar) {
-										err = 1;
-										break;
-									}
-									INSERT_BITS((*ip).insn_opcode, 0x1, OP_MASK_AQ, OP_SH_AQ);
-									break;
-								case 'f':
-									if(prev == 't' || prev == 'f' || is_scalar) {
-										err = 1;
-										break;
-									}
-									INSERT_BITS((*ip).insn_opcode, 0x1, OP_MASK_AQ, OP_SH_AQ);
-									INSERT_BITS((*ip).insn_opcode, 0x1, OP_MASK_RL, OP_SH_RL);
-									break;
-								default: err = 1;
-							}
-							if(err) break;
-							prev = *(s + 1);
-							s += 2;
-						}
-						if(err) {
-							as_bad(_("bad RVV register modifier'\n"));
-							break;
 						}
 						continue;
 					default:
